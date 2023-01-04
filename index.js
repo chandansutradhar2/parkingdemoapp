46da70f0ca768f4cfceaa50f6ede6c39860335c5
chandan <chandansutradhar2@gmail.com>
2023-01-04 14:59:11 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function fun1() {
+  setTimeout(() => {
+    console.log("function 1 invoked");
+  }, 1000);
+	
+}
+
+function fun2() {
+  setTimeout(() => {
+    console.log("function 2 invoked");
+  }, 1000);
+	
+}
+
